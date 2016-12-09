6176aa5b3701d513baab9096c13e00d0ca34afa1
Ilham Imaduddin <ilhamadun@gmail.com>
2016-12-10 00:51:18 +0700

    Create PredictionActivity
    This activity shows:
    - Currently predicted activity (as a CardView)
    - Activity history (as a RecyclerView)
    
    The cross icon in activity history item is implemented as item deletion. Later, it should prompt user to choose the right activity. As for item deletion, the user can swipe the card.

---
+++ app/src/main/java/org/elins/aktvtas/HumanActivityHistoryAdapter.java
@@ -0,0 +1,81 @@
+package org.elins.aktvtas;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class HumanActivityHistoryAdapter
+        extends RecyclerView.Adapter<HumanActivityHistoryAdapter.ActivityViewHolder> {
+
+    List<HumanActivity> humanActivities = new ArrayList<>();
+
+    public static class ActivityViewHolder extends RecyclerView.ViewHolder {
+        FrameLayout frame;
+        ImageView activityIcon;
+        TextView activityName;
+        TextView activityInfo;
+        ImageView wrongButton;
+
+        ActivityViewHolder(View view) {
+            super(view);
+            frame = (FrameLayout) view.findViewById(R.id.activity_history_card);
+            activityIcon = (ImageView) view.findViewById(R.id.activity_history_icon);
+            activityName = (TextView) view.findViewById(R.id.activity_history_name);
+            activityInfo = (TextView) view.findViewById(R.id.activity_history_info);
+            wrongButton = (ImageView) view.findViewById(R.id.activity_history_wrong_button);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return humanActivities.size();
+    }
+
+    @Override
+    public ActivityViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.human_activity_history_card, viewGroup, false);
+
+        return new ActivityViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ActivityViewHolder activityViewHolder, int i) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm", Locale.getDefault());
+        HumanActivity activity = humanActivities.get(i);
+
+        final Date startTime = activity.getStartTime().getTime();
+        final Date endTime = activity.getEndTime().getTime();
+
+        activityViewHolder.activityIcon.setImageResource(humanActivities.get(i).getIcon());
+        activityViewHolder.activityName.setText(humanActivities.get(i).getName());
+        activityViewHolder.activityInfo
+                .setText(String.format("%s - %s", dateFormat.format(startTime), dateFormat.format(endTime)));
+        activityViewHolder.wrongButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int position = activityViewHolder.getAdapterPosition();
+                humanActivities.remove(position);
+                notifyItemRemoved(position);
+            }
+        });
+    }
+
+    public int addItem(HumanActivity humanActivity) {
+        humanActivities.add(humanActivity);
+        notifyItemInserted(humanActivities.size() - 1);
+
+        return humanActivities.size();
+    }
+}
---
+++ app/src/main/java/org/elins/aktvtas/PredictionActivity.java
@@ -0,0 +1,40 @@
+package org.elins.aktvtas;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class PredictionActivity extends AppCompatActivity {
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, PredictionActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_prediction);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView activityHistory = (RecyclerView) findViewById(R.id.human_activity_history);
+        activityHistory.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        activityHistory.setLayoutManager(layoutManager);
+
+        HumanActivityHistoryAdapter adapter = new HumanActivityHistoryAdapter();
+        activityHistory.setAdapter(adapter);
+
+        adapter.addItem(new HumanActivity(this, HumanActivityRegister.ActivityId.DRIVE));
+        adapter.addItem(new HumanActivity(this, HumanActivityRegister.ActivityId.STAND));
+        adapter.addItem(new HumanActivity(this, HumanActivityRegister.ActivityId.RIDE));
+    }
+}
