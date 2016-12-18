46efc41235f50fbec92a144ae31dbbaae5f73dcb
Ilham Imaduddin <ilhamadun@gmail.com>
2016-12-18 13:51:26 +0700

    Create PredictionService
    PredictionService is a service to make prediction from sensor data. It is **NOT** ready to use yet. Some method is not yet implemented. This service also calls a native method to classify activity with TensorFlow (also not yet implemented).
    
    To avoid diplication,  SensorService is created as a base of LogSensorService and Prediction service., which both need a similar sensor reading routine.

---
+++ app/src/main/java/org/elins/aktvtas/sensor/SensorService.java
@@ -0,0 +1,90 @@
+package org.elins.aktvtas.sensor;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import org.elins.aktvtas.sensor.SensorData;
+import org.elins.aktvtas.sensor.SensorDataSequence;
+import org.elins.aktvtas.sensor.SensorReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SensorService extends Service implements SensorReader.SensorReaderEvent {
+    public static final String SENSOR_TO_READ = "org.elins.aktvtas.extra.SENSOR_TO_READ";
+
+    protected List<Integer> sensorToRead = new ArrayList<>();
+    protected List<Integer> numberOfAxis = new ArrayList<>();
+
+    protected SensorReader sensorReader;
+    protected SensorDataSequence sensorDataSequence;
+    protected SensorDataWriter sensorDataWriter;
+
+    private List<SensorData> buffer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Should be used as base class only");
+    }
+
+    @Override
+    public void onSensorDataReady() {
+        buffer = sensorReader.read();
+        if (buffer != null) {
+            for (SensorData data : buffer) {
+                sensorDataSequence.setData(data);
+            }
+            sensorDataSequence.commit();
+        }
+    }
+
+    protected void writeBuffer() {
+        if (sensorDataSequence.size() % 1500 == 0) {
+            writeLog();
+            sensorDataSequence.clear();
+        }
+    }
+
+    public List<SensorData> getLastSensorData() {
+        if (sensorDataSequence.size() > 0) {
+            return sensorDataSequence.getLastData();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    protected void extractSensorToRead(int[] sensors) {
+        for (int sensor : sensors) {
+            sensorToRead.add(sensor);
+            numberOfAxis.add(3);
+        }
+    }
+
+    protected void createSensorDataReader(List<Integer> sensorToRead) {
+        sensorReader = new SensorReader(this, sensorToRead);
+        sensorReader.enableEventCallback(this);
+    }
+
+    protected void createSensorDataSequence(List<Integer> sensorToRead, List<Integer> numberOfAxis) {
+        sensorDataSequence = new SensorDataSequence();
+        for (int i = 0; i < sensorToRead.size(); i++) {
+            SensorData sensorData = new SensorData(sensorToRead.get(i), numberOfAxis.get(i));
+            sensorDataSequence.registerSensor(sensorData);
+        }
+    }
+
+    protected void createSensorDataWriter(String filename) {
+        String basePath = getExternalFilesDir(null).getAbsolutePath();
+        String filePath = basePath + "/" + filename + ".csv";
+        sensorDataWriter = new SensorDataWriter(filePath);
+    }
+
+    protected void writeLog() {
+        sensorDataWriter.open();
+        sensorDataWriter.write(sensorDataSequence);
+        sensorDataWriter.close();
+    }
+}
