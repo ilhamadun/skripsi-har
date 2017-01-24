99309f63a61b159a758412b66060051364510c66
Ilham Imaduddin <ilhamadun@gmail.com>
2017-01-24 10:03:30 +0700

    Create preference for DeviceIdentifier
    This preference will store device's id and token, which is needed to upload log file to server. Later, I will implement an Activity for registering device, which will be started if both preference is empty.

---
+++ app/src/main/java/org/elins/aktvtas/preferences/DeviceIdentifier.java
@@ -0,0 +1,22 @@
+package org.elins.aktvtas.preferences;
+
+public class DeviceIdentifier {
+    private String device;
+    private String token;
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ app/src/main/java/org/elins/aktvtas/preferences/Preferences.java
@@ -0,0 +1,30 @@
+package org.elins.aktvtas.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Preferences {
+    public static final String PREFERENCES = "org.example.aktvtas.preferences";
+    public static final String DEVICE = "org.example.aktvtas.preferences.device";
+    public static final String TOKEN = "org.example.aktvtas.preferences.token";
+
+    public static void registerDevice(Context context, String device, String token) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(DEVICE, device);
+        editor.putString(TOKEN, token);
+        editor.apply();
+    }
+
+    public static DeviceIdentifier getDeviceIdentifier(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFERENCES,
+                Context.MODE_PRIVATE);
+
+        DeviceIdentifier deviceIdentifier = new DeviceIdentifier();
+        deviceIdentifier.setDevice(sharedPreferences.getString(DEVICE, ""));
+        deviceIdentifier.setToken(sharedPreferences.getString(TOKEN, ""));
+
+        return deviceIdentifier;
+    }
+}
