b282f56aa938448dcb7f0f30dd05bfd7df25a499
Ilham Imaduddin <ilhamadun@gmail.com>
2017-05-30 09:25:27 +0700

    Remove unnecessary functions
    Whats removed:
    - Unused menu
    - HomeFragment

--- aktvtas/app/src/main/java/org/elins/aktvtas/MainActivity.java
+++ aktvtas/app/src/main/java/org/elins/aktvtas/MainActivity.java
@@ -15,22 +15,16 @@ import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-
-import org.elins.aktvtas.preferences.Preferences;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int DRAWER_ID_HOME = 0;
     private static final int DRAWER_ID_PREDICTION = 1;
     private static final int DRAWER_ID_TRAIN = 2;
-    private static final int DRAWER_ID_SETTINGS = 3;
-    private static final int DRAWER_ID_HELP = 4;
-    private static final int DRAWER_ID_ABOUT = 5;
+
+    private ActivityChooserFragment activityChooserFragment;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
@@ -42,45 +36,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (! Preferences.deviceIsRegistered(this)) {
-            RegisterActivity.startActivity(this);
-        }
-
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.prediction);
 
         Drawer drawer = setupNavigationDrawer(toolbar);
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        Fragment homeFragment = HomeFragment.newInstance();
-        transaction.add(R.id.content, homeFragment).commit();
+        activityChooserFragment = ActivityChooserFragment.newInstance();
+        transaction.replace(R.id.content, activityChooserFragment).commit();
     }
 
     private Drawer setupNavigationDrawer(Toolbar toolbar) {
         AccountHeader accountHeader = new AccountHeaderBuilder().withActivity(this).build();
-        PrimaryDrawerItem home = new PrimaryDrawerItem().withIdentifier(DRAWER_ID_HOME)
-                .withName(R.string.home);
         PrimaryDrawerItem prediction = new PrimaryDrawerItem().withIdentifier(DRAWER_ID_PREDICTION)
                 .withName(R.string.prediction);
         PrimaryDrawerItem train = new PrimaryDrawerItem().withIdentifier(DRAWER_ID_TRAIN)
                 .withName(R.string.train);
-        SecondaryDrawerItem settings = new SecondaryDrawerItem().withIdentifier(DRAWER_ID_SETTINGS)
-                .withName(R.string.settings);
-        SecondaryDrawerItem help = new SecondaryDrawerItem().withIdentifier(DRAWER_ID_HELP)
-                .withName(R.string.help);
-        SecondaryDrawerItem about = new SecondaryDrawerItem().withIdentifier(DRAWER_ID_ABOUT)
-                .withName(R.string.about);
 
         return new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .withAccountHeader(accountHeader)
                 .addDrawerItems(
-                        home, prediction, train,
-                        new DividerDrawerItem(),
-                        settings, help, about
+                        prediction, train
                 )
                 .withOnDrawerItemClickListener(drawerItemClickListener())
                 .build();
@@ -93,20 +74,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-                if (drawerItem.equals(DRAWER_ID_HOME)) {
-                    Fragment predictionFragment = HomeFragment.newInstance();
-                    transaction.replace(R.id.content, predictionFragment).commit();
-                } else if (drawerItem.equals(DRAWER_ID_PREDICTION)) {
-                    PredictionActivity.startActivity(getApplicationContext());
+                if (drawerItem.equals(DRAWER_ID_PREDICTION)) {
+                    getSupportActionBar().setTitle(R.string.prediction);
+                    activityChooserFragment.setMode(ActivityChooserFragment.MODE_PREDICTION);
                 } else if (drawerItem.equals(DRAWER_ID_TRAIN)) {
-                    Fragment trainingChooserFragment = TrainingChooserFragment.newInstance();
-                    transaction.replace(R.id.content, trainingChooserFragment).commit();
-                } else if (drawerItem.equals(DRAWER_ID_SETTINGS)) {
-                    Snackbar.make(view, R.string.settings, Snackbar.LENGTH_SHORT).show();
-                } else if (drawerItem.equals(DRAWER_ID_HELP)) {
-                    Snackbar.make(view, R.string.help, Snackbar.LENGTH_SHORT).show();
-                } else if (drawerItem.equals(DRAWER_ID_ABOUT)) {
-                    Snackbar.make(view, R.string.about, Snackbar.LENGTH_SHORT).show();
+                    getSupportActionBar().setTitle(R.string.train);
+                    activityChooserFragment.setMode(ActivityChooserFragment.MODE_TRAINING);
                 }
 
                 return false;
