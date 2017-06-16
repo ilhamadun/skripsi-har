04f9f9364a465b731f9d89957df8e0cf87c467a4
Ilham Imaduddin <ilhamadun@gmail.com>
2017-06-17 00:29:39 +0700

    Remove comments for more compact latex document

--- har/data.py
+++ har/data.py
@@ -2,31 +2,27 @@ import tensorflow as tf
 import numpy as np
 
 
-def get(filenames, num_target, window_size, overlap=0.5):
-    """ Get data and prepares it for training and testing
-
-    This method do all the preparation needed for training and testing process,
-    including loading, shuffling and dividing data to train and test group
-
-    filenames   -- list of filename to open
-    num_target  -- number of target class
-    window_size -- size of window to group the data
-    overlap     -- overlap size of the sliding window
-    divider     -- ratio for dividing train and test data
-    """
+def get(filenames, num_target, window_size, overlap=0.5, divider=None, one_hot=True):
     data, target = load(filenames, window_size, overlap)
     data, target = shuffle(data, target)
 
-    return data, tf.one_hot(target, num_target)
+    if divider:
+        data_train, data_test = divide(data, divider)
+        target_train, target_test = divide(target, divider)
+
+        if one_hot:
+            target_train = tf.one_hot(target_train, num_target)
+            target_test = tf.one_hot(target_test, num_target)
+
+        return data_train, data_test, target_train, target_test
+    else:
+        if one_hot:
+            target = tf.one_hot(target, num_target)
+
+        return data, target
 
 
 def load(filenames, window_size, overlap):
-    """ Load csv files
-
-    filenames   -- list of filename to open
-    window_size -- size of window to group the data
-    overlap     -- overlap size of the sliding window
-    """
     data = None
     target = None
 
@@ -49,11 +45,6 @@ def load(filenames, window_size, overlap):
 
 
 def sliding_window(data_in, window_size, overlap = 0.5):
-    """ Apply sliding windows to data
-
-    data_in     -- data array
-    window_size -- size of window to group the data
-    """
     step = int(window_size * (1 - overlap))
     data, target = [], []
 
@@ -68,11 +59,6 @@ def sliding_window(data_in, window_size, overlap = 0.5):
 
 
 def shuffle(data, target):
-    """ Shuffle the data
-
-    data   -- data to shuffle
-    target -- target to shuffle
-    """
     target = np.reshape(target, [target.shape[0], 1])
     sample = np.concatenate((data, target), axis=1)
     np.random.shuffle(sample)
@@ -84,11 +70,6 @@ def shuffle(data, target):
 
 
 def divide(arr, divider):
-    """ Divide data to train annd test group
-
-    arr      -- array of data to divide
-    divider  -- [divider] ratio goes to train data, the rest goes to test data
-    """
     train = arr[0:int(arr.shape[0] * divider)]
     test = arr[int(arr.shape[0] * divider):]
 
